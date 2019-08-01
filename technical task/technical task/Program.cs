c2e466f40bb7bc8b8ce16196afa630ed3886a21e
vadimNarbut <47208704+vadimNarbut@users.noreply.github.com>
2019-08-01 14:21:28 +0300

    Add files via upload

---
+++ technical task/technical task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace technical_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> List1 = new List<int>() { 1, 2, 2, 3 };
+            List<int> List2 = new List<int>() { 11, 3, 1, 5, -9, 11 };
+
+            ListResult(List1, List2);
+            Console.WriteLine();
+        }
+
+        static void ListResult(List<int> L1, List<int> L2)
+        {
+            var resultList = L1.Concat(L2);
+            resultList = resultList.OrderBy(i => i).ToList();
+            foreach (int i in resultList.Distinct())
+            {
+                Console.Write(" " + i + " ");
+            }
+        }
+
+    }
+}
